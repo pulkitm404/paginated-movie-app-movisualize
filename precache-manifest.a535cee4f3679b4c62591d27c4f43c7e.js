35613e31c2ba615895f5c6a344bbd1c00f149881
Pulkit Mehta <pulkitm404@gmail.com>
2020-04-26 11:26:24 +0530

    Updates

---
+++ precache-manifest.a535cee4f3679b4c62591d27c4f43c7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2534dba2536b1692744f97a188f5a86b",
+    "url": "/paginated-movie-app-movisualize/index.html"
+  },
+  {
+    "revision": "021f72c5813cc399ab6b",
+    "url": "/paginated-movie-app-movisualize/static/css/main.fc5d5ffa.chunk.css"
+  },
+  {
+    "revision": "d04f96c83ee30fc21c92",
+    "url": "/paginated-movie-app-movisualize/static/js/2.441a81c8.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/paginated-movie-app-movisualize/static/js/2.441a81c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "021f72c5813cc399ab6b",
+    "url": "/paginated-movie-app-movisualize/static/js/main.4e4c344f.chunk.js"
+  },
+  {
+    "revision": "d62f497d15a6b6a8402c",
+    "url": "/paginated-movie-app-movisualize/static/js/runtime-main.34756044.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/paginated-movie-app-movisualize/precache-manifest.a535cee4f3679b4c62591d27c4f43c7e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paginated-movie-app-movisualize/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
